753ed51e6558d698cf36240431e17af385000a5a
KG <krisgolcz@gmail.com>
2024-11-06 22:40:34 +0000

    pray

--- fe-config.sh
+++ fe-config.sh
@@ -15,28 +15,38 @@ sudo apt install nginx -y
 
 cd /home/azuser
 
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.2/install.sh | bash
-export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" # This loads nvm
-
-
-nvm install 12.11.1
-
 git clone https://github.com/spring-petclinic/spring-petclinic-angular.git
 
 cd spring-petclinic-angular/
 sed -i "s/localhost/$FE_URL/g" src/environments/environment.ts src/environments/environment.prod.ts
 sed -i "s/9966/8080/g" src/environments/environment.ts src/environments/environment.prod.ts
 
+
+sudo -s
+
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.2/install.sh | bash
+export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" # This loads nvm
+
+# source ~/.bashrc
+nvm install 18.19
+nvm use 18.19
+
 echo Log: | npm install -g @angular/cli@latest
 echo Log: | npm install
 echo Log: | ng analytics off
 
+
+# ng build --prod --base-href=/petclinic/ --deploy-url=/petclinic/
 ng build --prod --base-href=/petclinic/ --deploy-url=/petclinic/
+
+# sudo npm install @angular/cli@11.2.11 --save-dev #install locally 
+# sudo npx ng build --prod --base-href=/petclinic/ --deploy-url=/petclinic/ # build locally
+
 
 sudo mkdir /usr/share/nginx/html/petclinic
 
-sudo cp -r dist/spring-petclinic-angular/* /usr/share/nginx/html/petclinic
+sudo cp -r dist/ /usr/share/nginx/html/petclinic
 
 cat > petclinic.conf << EOL
 server {
